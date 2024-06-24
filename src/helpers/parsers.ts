7b0849301a9df6a6dd9c5bdd33b71af46394e8a7
probaton <probatos@gmail.com>
2024-06-24 14:31:51 -0600

    Correctly space and parse help-text for args with multiple flags

--- src/helpers/parsers.ts
+++ src/helpers/parsers.ts
@@ -1,12 +1,37 @@
-function parseArgText(arg: IHalpArg): string {
-  let argText = typeof arg.flag == 'string' ? ` -${arg.flag}` : `  ${arg.flag}`;
-  argText += arg.requiredMessage ? '*' : '';
-  while (argText.length < 6) {
-    argText += ' ';
+function parseFlagText(flag: IHalpArgFlag): string {
+  switch (typeof flag) {
+    case 'string': return flag.length === 1 ? `-${flag}` : `--${flag}`;
+    case 'number': return ` ${flag}`;
+    case 'object': return flag.map(parseFlagText).join(',');
   }
-  argText += arg.helpText || '';
-  argText += ('default' in arg) ? ` (default: <${arg.default}>)` : '';
-  return argText;
+}
+
+function parseArgsText(args?: IHalpArg[]): string {
+  if (!args || args.length === 0) return '';
+
+  let argsText = '\n\nOptions\n';
+
+  let hasRequiredArgs = false;
+  let maxLength = 0;
+  const flagTexts: string[] = [];
+  for (const arg of args) {
+    hasRequiredArgs = hasRequiredArgs || !!(arg.requiredMessage);
+    const flagText = `${parseFlagText(arg.flag)}${arg.requiredMessage ? '*' : ''}`;
+    maxLength = Math.max(maxLength, flagText.length);
+    flagTexts.push(flagText);
+  }
+      
+  if (hasRequiredArgs) {
+    argsText += 'Options marked * are required\n';
+  }
+
+  for (let i = 0; i < args.length; i++) {
+    const spacedFlagText = `${flagTexts[i]}${' '.repeat(maxLength - flagTexts[i].length)}`;
+    const defaultText = ('default' in args[i]) ? ` (default: <${args[i].default}>)` : '';
+    argsText += `${spacedFlagText} ${args[i].helpText || ''}${defaultText}\n`;
+  }
+
+  return argsText;
 }
 
 export function parseHelpText(action: IHalper): string {
@@ -16,16 +41,7 @@ export function parseHelpText(action: IHalper): string {
 
   let helpText = action.command;
   helpText += action.helpText ? `\n${action.helpText}` : '';
-
-  if (action.args && action.args.length > 0) {
-    helpText += '\n\nOptions\n';
-    if (action.args.some(arg => arg.requiredMessage)) {
-      helpText += 'Options marked * are required\n';
-    }
-    helpText += action.args
-      .map(parseArgText)
-      .reduce((aggregate, argText) => `${aggregate}\n${argText}`);
-  }
+  helpText += parseArgsText(action.args);
   return helpText;
 }
 
