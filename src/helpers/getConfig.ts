49886f190001db95203681aa63f77cd1e6353050
probaton <probatos@gmail.com>
2024-09-18 08:42:29 -0600

    Abstract yargs env parsing into helper function

--- src/helpers/getConfig.ts
+++ src/helpers/getConfig.ts
@@ -7,21 +7,37 @@ export default function getConfig(): IHalpConfig {
 }
 
 export function setConfig(cache: ConfigCache) {
-  const envConfig = resolveEnvConfig(yargs.argv.e as string || 'dev');
+  const envConfig = resolveEnvConfig();
   const config = Object.assign({}, require('../../configs/base.conf.json'), envConfig);
   cache.set(config);
   return config;
 }
 
-function resolveEnvConfig(env: string | number): IHalpConfig {
-  switch(env) {
-    case 'd':
+function resolveEnvConfig(): IHalpConfig {
+  switch(getEnv()) {
     case 'dev':
       return require('../../configs/dev.conf.json');
-    case 'l':
     case 'local':
       return require('../../configs/local.conf.json');
     default:
       throw new RangeError('Valid options for -e are dev and local');
+  }
+}
+
+type Env = 'int' | 'dev' | 'local'; 
+
+export function getEnv(): Env {
+  switch(yargs.argv.e as string || 'dev') {
+    case 'i':
+    case 'int':
+      return 'int';
+    case 'd':
+    case 'dev':
+      return 'dev';
+    case 'l':
+    case 'local':
+      return 'local';
+    default:
+      throw new RangeError('Valid options for -e are int, dev, and local');
   }
 }
