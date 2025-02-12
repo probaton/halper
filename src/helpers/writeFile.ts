5df68d80e6f1d5923282c7c359f838a980f3f09f
probaton <probatos@gmail.com>
2025-02-12 15:31:57 -0700

    Make writeFile input-agnostic

--- src/helpers/writeFile.ts
+++ src/helpers/writeFile.ts
@@ -3,9 +3,9 @@ import { existsSync, mkdirSync, writeFileSync } from 'fs';
 import { getRootDirectory } from './util';
 
 // Writes fileString to halpers/exports/<fileName>
-export default function writeFile(fileName: string, fileString: string) {
+export default function writeFile(fileName: string, fileContents: any) {
   const filePath = `${getRootDirectory()}/exports`;
   !existsSync(filePath) && mkdirSync(filePath);
-  writeFileSync(`${filePath}/${fileName}`, JSON.stringify(fileString, undefined, 2));
+  writeFileSync(`${filePath}/${fileName}`, JSON.stringify(fileContents, undefined, 2));
   return `Output saved to\n${filePath}/${fileName}`;
 }
