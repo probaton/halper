8123f390ee358518eabcb4b522cefd8914ba4bd6
probaton <probatos@gmail.com>
2025-06-24 11:32:51 -0600

    Add toInclude to words flow

--- src/actions/words.halp.ts
+++ src/actions/words.halp.ts
@@ -7,19 +7,26 @@ function getDictionary(allWords: boolean) {
   return fs.readFileSync(`${getRootDirectory()}/assets/${fileName}.txt`, 'utf-8');
 }
 
-export function words(query: string, allWords: boolean, toExclude?: string) {
+export function words(query: string, allWords: boolean, toExclude?: string, toInclude?: string) {
   const regExp = new RegExp(`\n${query}\n`, 'g');
-  const preExcludeMatches = getDictionary(allWords).match(regExp);
-  const excludeArray = toExclude ? toExclude.split('') : [];
-  const matches = toExclude
-    ? preExcludeMatches?.filter(word => !word.split('').some(letter => excludeArray.includes(letter)))
-    : preExcludeMatches;
+  let matches = (getDictionary(allWords).match(regExp) ?? []) as string[];
+
+  if (toExclude)  {
+    const excludeArray = toExclude ? toExclude.split('') : [];
+    matches = matches.filter(word => !word.split('').some(letter => excludeArray.includes(letter)));
+  }
+
+  if (toInclude)  {
+    const includeArray = toInclude ? toInclude.split('') : [];
+    matches = matches.filter(w => includeArray.some(l => w.includes(l)));
+  }
+
   if (!matches || matches.length === 0) {
     return `No matches found for ${query}`;
   }
   return matches
     .reduce((aggregate, match) => `${aggregate}${match.substring(0, match.length - 1)}`, '')
-    .substring(1);
+    .trim();
 }
 
 const halpConfig: IHalper = {
@@ -30,6 +37,7 @@ const halpConfig: IHalper = {
     { flag: 1, helpText: 'Regex search query', requiredMessage: 'Missing search query' },
     { flag: 'a', helpText: 'Search ALL English words instead of just the dictionary', default: false },
     { flag: 'x', helpText: 'Letters to exclude' },
+    { flag: 'i', helpText: 'Letters to include' },
   ],
 };
 export default halpConfig;
